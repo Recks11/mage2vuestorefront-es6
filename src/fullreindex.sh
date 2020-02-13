7c07080b47062fe873c88350053fc42f1a6e82e2
Ijiekhuamen Rex <dev.rexijie@gmail.com>
2020-02-13 15:14:34 +0100

    dependency updates

---
+++ src/fullreindex.sh
@@ -0,0 +1,6 @@
+node --harmony cli.js reviews
+node --harmony cli.js categories --removeNonExistent=true --extendedCategories=true
+node --harmony cli.js productcategories
+node --harmony cli.js attributes --removeNonExistent=true
+node --harmony cli.js taxrule --removeNonExistent=true
+node --harmony cli.js products --removeNonExistent=true --partitions=1
